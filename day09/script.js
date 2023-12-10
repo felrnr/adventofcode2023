aaab9ef1efa499439a6d4a003b256d30db58abf7
Janko <janko.chavannes@riviq.nl>
2023-12-10 11:06:40 +0100

    day09 p2

--- day09/script.js
+++ day09/script.js
@@ -1,7 +1,7 @@
 var fs = require('fs'),
   path = require('path');
 
-const filepath = path.join(__dirname, "input_test.txt");
+const filepath = path.join(__dirname, "input.txt");
 const data = fs.readFileSync(filepath).toString();
 
 const inputs = data.split('\n').map(line => line.split(' ').map(Number));
@@ -11,7 +11,7 @@ function buildDerivatives(readings=[1]) {
     let stack = [[...readings]];
     let layer = 0;
     while (stack[layer].some(v => v !== 0)) {
-        let newLayer = [...Array(stack[layer].length-1).keys()].map(i => stack[layer][i+1] - stack[layer][i]);
+        const newLayer = [...Array(stack[layer].length-1).keys()].map(i => stack[layer][i+1] - stack[layer][i]);
         layer++;
         stack[layer] = newLayer;
         if (newLayer.length === 0) throw Error("Woops 0 length derivative left...");
@@ -27,24 +27,30 @@ function printPyramid(stack) {
     );
 }
 
-function getNextValue(readings) {
+function getNextValue(readings, mode="right") {
     let stacks = buildDerivatives(readings);
-    const N = stacks.length;
-    stacks.at(-1).push(0);
+    if (mode==="right") stacks.at(-1).push(0);
+    else stacks.at(-1).unshift(0);
+
     stacks.forEach((_, idx) => {
         if (idx === 0) return;
-        stacks[N - (idx+1)].push(stacks[N - (idx+1)].at(-1) + stacks[N-(idx)].at(-1));
+        if (mode === "right")
+            stacks.at(-(idx+1)).push(stacks.at(-(idx+1)).at(-1) + stacks.at(-idx).at(-1));
+        else
+            stacks.at(-(idx+1)).unshift(stacks.at(-(idx+1))[0] - stacks.at(-idx)[0]);
     });
+
     // printPyramid(stacks);
-    return stacks[0].at(-1);
+    return (mode === "right") ? stacks[0].at(-1) : stacks[0][0];
 }
 
-let p1 = inputs.map((readings, idx) => {
-    // console.log(`Readings ${idx}`)
-    // printPyramid(buildDerivatives(readings));
-
-    return getNextValue(readings)
-}).reduce((a,b) => a+b);
-
+const p1 = inputs
+    .map(readings => getNextValue(readings))
+    .reduce((a,b) => a+b);
 console.log(p1);
 
+// Part 2
+const p2 = inputs
+    .map(readings => getNextValue(readings, mode="left"))
+    .reduce((a,b) => a+b);
+console.log(p2);
